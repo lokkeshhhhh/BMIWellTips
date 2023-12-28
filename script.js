5a0b924a1b3aaeafad687a87c0b59cdbd3c0fe7d
Lokesh Kumawat <lokesh8250kumawat@gmail.com>
2023-12-28 07:46:30 +0530

    script added and about's display solved

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("home-btn").addEventListener('click', () => {
+    document.getElementById("about").style.display = 'none';
+})
+document.getElementById("contacts-btn").addEventListener('click', () => {
+    document.getElementById("about").style.display = 'none';
+})
+document.getElementById("services-btn").addEventListener('click', () => {
+    document.getElementById("about").style.display = 'none';
+})
+document.getElementById("blogs-btn").addEventListener('click', () => {
+    document.getElementById("about").style.display = 'none';
+})
+
+document.getElementById("about-btn").addEventListener('click', () => {
+    document.getElementById("about").style.display = '';
+})
+
