6a80081cae4ac6281070a8347386ae720705b131
Lokesh Kumawat <lokesh8250kumawat@gmail.com>
2023-12-28 10:33:17 +0530

    Footer links done

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ document.getElementById("about-bmi").addEventListener('click', () => {
 document.getElementById("about-developer").addEventListener('click', () => {
     document.getElementById("blogs-btn").click();
 })
+
+
+document.getElementById("f-home").addEventListener('click', () => {
+    document.getElementById("home-btn").click();
+})
+document.getElementById("f-services").addEventListener('click', () => {
+    document.getElementById("services-btn").click();
+})
+document.getElementById("f-contacts").addEventListener('click', () => {
+    document.getElementById("contacts-btn").click();
+})
+
+let linksAbout = document.querySelectorAll("#f-about");
+
+for(let i=0; i<linksAbout.length; i++){
+    linksAbout[i].addEventListener('click', () => {
+        document.getElementById("about-btn").click();
+    })
+}
+let linksBlogs = document.querySelectorAll("#f-blogs");
+for(let i=0; i<linksBlogs.length; i++){
+    linksBlogs[i].addEventListener('click', () => {
+        document.getElementById("blogs-btn").click();
+    })
+}
