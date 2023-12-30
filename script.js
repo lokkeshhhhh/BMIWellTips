10901dc7366d29b4a146381890067171a785d7db
Lokesh Kumawat <lokesh8250kumawat@gmail.com>
2023-12-30 21:09:20 +0530

    new fade effect added

--- script.js
+++ script.js
@@ -14,6 +14,37 @@ document.getElementById("blogs-btn").addEventListener('click', () => {
 
 document.getElementById("about-btn").addEventListener('click', () => {
     document.getElementById("about").style.display = '';
+
+
+    const elementsToLoad = document.querySelectorAll('.card, .about-pg');
+
+
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+
+        return (
+            (rect.top <= windowHeight / 2 && rect.bottom >= windowHeight / 2) ||
+            (rect.top >= 0 && rect.bottom <= windowHeight)
+        );
+    }
+
+
+    function handleScroll() {
+        elementsToLoad.forEach((element) => {
+            if (isInViewport(element) && !element.classList.contains('loaded')) {
+                element.classList.add('loaded');
+            }
+        });
+    }
+
+
+    handleScroll();
+
+
+    window.addEventListener('scroll', handleScroll);
+
 })
 
 document.getElementById("about-bmi").addEventListener('click', () => {
@@ -37,14 +68,16 @@ document.getElementById("f-contacts").addEventListener('click', () => {
 
 let linksAbout = document.querySelectorAll("#f-about");
 
-for(let i=0; i<linksAbout.length; i++){
+for (let i = 0; i < linksAbout.length; i++) {
     linksAbout[i].addEventListener('click', () => {
         document.getElementById("about-btn").click();
     })
 }
 let linksBlogs = document.querySelectorAll("#f-blogs");
-for(let i=0; i<linksBlogs.length; i++){
+for (let i = 0; i < linksBlogs.length; i++) {
     linksBlogs[i].addEventListener('click', () => {
         document.getElementById("blogs-btn").click();
     })
 }
+
+
