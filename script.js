1a76c4ac954710ce6b67294af7f6fd1360a99e8d
Lokesh Kumawat <lokesh8250kumawat@gmail.com>
2024-01-04 09:02:48 +0530

    document formated

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("about").style.display = 'none';
 
 const elementsToLoad = document.querySelectorAll('.health-quote, .health-quote-2, .bmi-btn,.method-cards');
 
-// check home section's video-contents are in viewport or not
+
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
@@ -15,10 +15,9 @@ function isInViewport(element) {
     );
 }
 
-// handle fade effect(add loaded class on loading/scrolling)
+
 function handleScroll() {
     elementsToLoad.forEach((element) => {
-        // check isInViewport()'s return and loaded class's presence
         if (isInViewport(element) && !element.classList.contains('loaded')) {
             element.classList.add('loaded');
         }
@@ -27,7 +26,6 @@ function handleScroll() {
 
 handleScroll();
 
-// call the fn on scrolling
 window.addEventListener('scroll', handleScroll);
 
 
@@ -36,19 +34,15 @@ document.getElementById("home-btn").addEventListener('click', () => {
     document.getElementById("about").style.display = 'none';
 })
 
-// change about sections display to none again
 document.getElementById("contacts-btn").addEventListener('click', () => {
     document.getElementById("about").style.display = 'none';
 })
 
-// change about sections display to none again
 document.getElementById("services-btn").addEventListener('click', () => {
     document.getElementById("about").style.display = 'none';
-    
 
     const elementsToLoad = document.querySelectorAll('.sr-cards');
 
-    // check home section's video-contents are in viewport or not
     function isInViewport(element) {
         const rect = element.getBoundingClientRect();
         const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
@@ -59,10 +53,10 @@ document.getElementById("services-btn").addEventListener('click', () => {
         );
     }
 
-    // handle fade effect(add loaded class on loading/scrolling)
+
     function handleScroll() {
         elementsToLoad.forEach((element) => {
-            // check isInViewport()'s return and loaded class's presence
+
             if (isInViewport(element) && !element.classList.contains('loaded')) {
                 element.classList.add('loaded');
             }
@@ -71,7 +65,6 @@ document.getElementById("services-btn").addEventListener('click', () => {
 
     handleScroll();
 
-    // call the fn on scrolling
     window.addEventListener('scroll', handleScroll);
 
 })
@@ -84,12 +77,10 @@ document.getElementById("blogs-btn").addEventListener('click', () => {
 
 // add some fade effects on about sections loading
 document.getElementById("about-btn").addEventListener('click', () => {
-    // make about section visible
     document.getElementById("about").style.display = '';
 
     const elementsToLoad = document.querySelectorAll('.about-pg');
 
-    // check about section's pages are in viewport or not
     function isInViewport(element) {
         const rect = element.getBoundingClientRect();
         const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
@@ -100,12 +91,11 @@ document.getElementById("about-btn").addEventListener('click', () => {
         );
     }
 
-    // handle fade effect(add loaded class on loading/scrolling)
+
     function handleScroll() {
         elementsToLoad.forEach((element) => {
             const texts = document.querySelectorAll("#second-1 ,#second-2");
 
-            // check isInViewport()'s return and loaded class's presence
             if (isInViewport(element) && !element.classList.contains('loaded')) {
                 element.classList.add('loaded');
 
@@ -115,7 +105,6 @@ document.getElementById("about-btn").addEventListener('click', () => {
                     texts[0].style.transform = "translateX(0px)";
                 }
 
-                // text's fade
                 if (elementsToLoad[1].classList.contains('loaded')) {
                     texts[1].style.opacity = 1;
                     texts[1].style.transform = "translateX(0px)";
@@ -126,7 +115,6 @@ document.getElementById("about-btn").addEventListener('click', () => {
 
     handleScroll();
 
-    // call the fn on scrolling
     window.addEventListener('scroll', handleScroll);
 
 })
@@ -137,40 +125,47 @@ document.getElementById("about-bmi").addEventListener('click', () => {
     document.getElementById("home-btn").click();
 })
 
-// redirect to blogs
-const aboutDev = document.querySelectorAll("#about-developer");
-for(let i=0;i<aboutDev.length;i++){
-    aboutDev[i].addEventListener('click', () => {
-        document.getElementById("blogs-btn").click();
-    })
-}
-
-// redirect to redirect to home
 document.getElementById("f-home").addEventListener('click', () => {
     document.getElementById("home-btn").click();
 })
-// redirect to services
-document.getElementById("f-services").addEventListener('click', () => {
-    document.getElementById("services-btn").click();
-})
-// redirect to contacts
-document.getElementById("f-contacts").addEventListener('click', () => {
-    document.getElementById("contacts-btn").click();
-})
 
-// redirect to home
 let linksAbout = document.querySelectorAll("#f-about");
 for (let i = 0; i < linksAbout.length; i++) {
     linksAbout[i].addEventListener('click', () => {
         document.getElementById("about-btn").click();
     })
 }
+
+
 // redirect to blogs
+const aboutDev = document.querySelectorAll("#about-developer");
+for (let i = 0; i < aboutDev.length; i++) {
+    aboutDev[i].addEventListener('click', () => {
+        document.getElementById("blogs-btn").click();
+    })
+}
+
 let linksBlogs = document.querySelectorAll("#f-blogs");
 for (let i = 0; i < linksBlogs.length; i++) {
     linksBlogs[i].addEventListener('click', () => {
         document.getElementById("blogs-btn").click();
     })
 }
+
+
+
+// redirect to services
+document.getElementById("f-services").addEventListener('click', () => {
+    document.getElementById("services-btn").click();
+})
+
+
+// redirect to contacts
+document.getElementById("f-contacts").addEventListener('click', () => {
+    document.getElementById("contacts-btn").click();
+})
+
+
+
 
 
